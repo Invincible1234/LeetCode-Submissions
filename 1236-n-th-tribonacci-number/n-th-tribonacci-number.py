33f54aced1b7b8ae3fa72fbcedde3a1ba15f4457
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2025-12-27 14:42:15 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.48%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -6,9 +6,8 @@ class Solution(object):
         f[2] = 1
 
         for i in range(3, n+1):
-             f[i] = f[i-1] + f[i-2] + f[i-3]
-
-
+            f[i] = f[i-1] + f[i-2] + f[i-3]
+            
         return f[n]
 
            
