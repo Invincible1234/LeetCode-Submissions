389a716f3a594f16152ada1dae60faf50fb122a0
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-01-04 13:06:01 -0800

    Time: 3 ms (29.02%) | Memory: 17.1 MB (99.62%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -6,7 +6,4 @@ class Solution:
         doubly = sum([freq for freq in nums if freq >= 10])
 
        
-        if doubly != singly:
-            return True
-        else:
-            return False
+        return singly != doubly
