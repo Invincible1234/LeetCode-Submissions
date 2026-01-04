210b9c85ceefa0414022d6b50b68b79d4d26c0b6
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-01-04 13:06:39 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (87.71%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -1,9 +1,9 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
         
-        singly = sum([element for element in nums if element < 10])
+        singly = [element for element in nums if element < 10]
 
-        doubly = sum([freq for freq in nums if freq >= 10])
+        doubly = [freq for freq in nums if freq >= 10]
 
        
-        return singly != doubly
+        return sum(singly) != sum(doubly)
