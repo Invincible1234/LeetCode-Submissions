30c61524c63423d76e520d32aeab116874a228d9
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-02-05 23:37:04 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (36.06%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+
+        stack = []
+        res = ""
+
+        for i in s:
+            if i.isdigit():
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+
+        return ''.join(stack)
+
+
