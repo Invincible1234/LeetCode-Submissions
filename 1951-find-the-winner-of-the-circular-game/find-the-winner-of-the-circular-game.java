a122923217205e2ce2958ba2a518af74fdc8b339
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-06-17 19:50:10 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (90.32%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,9 +1,13 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        int res = 0;
-        for(int i = 2; i <= n; i++){
-            res = (res + k) % i;
+        return recursion(n,k) + 1;
+    }
+    //Recursion solution. Same as bottom up DP but done recurssively, instead of a iteratively.
+    public int recursion(int n, int k){
+        if(n == 1){
+            return 0;
         }
-        return res + 1;
+
+        return (recursion(n - 1, k) + k) % n;
     }
 }
