f03850be65bba8f55d3a4d38155bda651b4d3b6f
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-06-21 19:22:47 -0700

    Time: 1 ms (96.18%) | Memory: 46.7 MB (79.88%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -17,32 +17,35 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
 
-        if(root == null){
+        if(root == null){ //checks if the root is null, then retures empty list.
             return res;
         }
+        //Since BFS uses Queue data structure, we use a linked list Queue to process everything.
+        Queue<TreeNode> queue = new LinkedList<>(); //Initialize the queue.
+        queue.offer(root); //add the first element which is the tree root.
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        while(!queue.isEmpty()){ //the while loop goes on until is is empty.
+            int size = queue.size(); //gets the size of the queue.
+            List<Integer> level = new ArrayList<>(); //this is the level by level list.
 
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < size; i++){ //for loop iterates until the size.
+                TreeNode current = queue.poll(); //returns the added value to current.
+                level.add(current.val); // add it to the level list.
 
-            for(int i = 0; i < size; i++){
-                TreeNode current = queue.poll();
-                level.add(current.val);
-
-                if(current.left != null){
+                if(current.left != null){ //checks if it has left child, if so add it to queue
                     queue.offer(current.left);
                 }
-                if(current.right != null){
+                if(current.right != null){ //checks if it has right child, if so add it to queue.
                     queue.offer(current.right);
                 }
             }
-            res.add(level);
+            res.add(level); // adds whatever element is in the level to the final list.
         }
-        return res;
-        
+        return res; //returns the 2d arraylist with every element contained.
+        /*Notes for self: Get better at handling queue data structure. BFS is level based.
+        * when traversing through bfs, have a level list to process each level.
+        * keep track of the queue size, big help.
+        */
 
     }
 }
