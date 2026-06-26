3f1e345160865015d46597d8817055a428d0d9f9
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-06-26 15:06:45 -0700

    Time: 39 ms (22.95%) | Memory: 86.5 MB (32.90%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,41 +1,40 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        //HeapSort.
+        int heap = nums.length; //get the length of the array
 
-        int n = nums.length;
-        for(int i = n/2; i >= 0; i--){
-            heapify(nums, n, i);
+        for(int i = heap/2 - 1; i >= 0; i--){ // start from the last parent and go upwards.
+            heapify(nums, heap, i); //heapify it
         }
 
-        for(int i = n-1; i > 0; i--){
-            int temp = nums[0];
-            nums[0] = nums[i];
-            nums[i] = temp;
-            heapify(nums, i, 0);
+        for(int i = heap - 1; i > 0; i--){//to sort it completely in a certain order swap the firt and last element.
+            int temp = nums[i];
+            nums[i] = nums[0];
+            nums[0] = temp;
+            heapify(nums, i, 0);//shrink the size by i once the last index is done.
         }
 
-        return nums;
+        return nums;//return the final result.
+
     }
 
-    private void heapify(int[] arr, int n, int i){
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-
-        if(left < n && arr[left] > arr[largest]){
-            largest = left;
+    public void heapify(int[] arr, int size, int i){ //arguments are array, it size and index.
+        int largest = i; //parent node
+        int left = 2 * i + 1; //left child
+        int right = 2 * i + 2; //right child (all property of heap)
+        //check if left is less than the size and if it is greater than its parent.
+        if(left < size && arr[left] > arr[largest]){
+            largest = left; //if so left is now the parent
+        }
+        //check if the right is lesss than the size and if it is greater than its parent.
+        if(right < size && arr[right] > arr[largest]){
+            largest = right; //if so right is now the parent.
         }
 
-        if(right < n && arr[right] > arr[largest]){
-            largest = right;
-        }
-
-        if(largest != i){
-            int temp = arr[i];
+        if(largest != i){ //if the largest is not equal to i after the swap with either left and right.
+            int temp = arr[i]; //do the swap after the comparision.
             arr[i] = arr[largest];
             arr[largest] = temp;
-            heapify(arr, n, largest);
+            heapify(arr, size, largest);//keep doing it until so.
         }
-
     }
 }
