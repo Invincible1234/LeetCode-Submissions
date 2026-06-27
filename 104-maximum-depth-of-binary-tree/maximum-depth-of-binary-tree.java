8fa5d794a5d4994623ca36840c1539fa14391f10
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-06-27 02:02:59 -0700

    Time: 1 ms (10.20%) | Memory: 44.8 MB (12.35%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,26 +15,26 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) { //BFS approach, notice vertices(circles) vs edges(paths).
-        if(root == null){
-            return 0;
+        if(root == null){ //check the root if it is null.
+            return 0;//if so, return 0;
         }
-        int level = 0;
-        Queue<TreeNode> q = new LinkedList();
-        q.offer(root);
+        int level = 0;//initiate a level checker with int level.
+        Queue<TreeNode> q = new LinkedList();//a queue of type TreeNode
+        q.offer(root);//add root to the queue.
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                TreeNode node = q.poll();
-                if(node.left != null){
+        while(!q.isEmpty()){//check where the q is empty, if not, continue.
+            int size = q.size();//set size of the queue.
+            for(int i = 0; i < size; i++){//iterate in the range of the size.
+                TreeNode node = q.poll();//return and store the last element in the queue.
+                if(node.left != null){//if the left is not null. store it in the queue.
                     q.offer(node.left);
                 }
-                if(node.right != null){
+                if(node.right != null){//if the right is not null. store it in the queue.
                     q.offer(node.right);
                 }
             }
-            level += 1;
+            level += 1;//increment the level by 1
         }
-        return level;
+        return level;//return it.
     }
 }
