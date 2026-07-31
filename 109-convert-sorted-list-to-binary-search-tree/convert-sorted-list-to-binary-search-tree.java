7240f14cbf4929cc15809f8060fc832b5acfd8f5
Jeet Vyas <62739171+Invincible1234@users.noreply.github.com>
2026-07-30 20:45:47 -0700

    Time: 1 ms (22.03%) | Memory: 47.3 MB (15.80%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> arr = new ArrayList<>();//create a new arrayList
+        while(head != null){//while the head is not null.
+            arr.add(head.val);//add every node to th array list
+            head = head.next;//iterate through it O(n).
+        }
+
+        return build(arr, 0, arr.size() - 1);//use the helper build function.
+    }
+
+    TreeNode build(List<Integer> l, int left, int right){//args is a list, left and right ints.
+        if(left > right) return null;//if left is greater than 
+        int mid = left + (right - left) / 2;
+
+        TreeNode root = new TreeNode(l.get(mid));
+        root.left = build(l, left, mid - 1);
+        root.right = build(l, mid + 1, right);
+        return root;
+    }
+}
